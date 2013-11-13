e14d4fadefa2936d00ad6e97b9ad27b639f7dff6
konradko <konrad.korzel@gmail.com>
2013-11-13 22:56:47 +0000

    Use form in all views, back to working state

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -91,7 +91,33 @@ class ListViewTest(TestCase):
         expected_error =  escape("You can't have an empty list item")
         self.assertContains(response, expected_error)
 
+    def test_displays_item_form(self):
+        list_ = List.objects.create()
+        response = self.client.get('/lists/%d/' % (list_.id,))
+        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertContains(response, 'name="text"')
+
 class NewListTest(TestCase):
+
+    def post_invalid_input(self):
+        list_ = List.objects.create()
+        return self.client.post(
+            '/lists/%d/' % (list_.id, ),
+            data={'text': ''}
+        )
+
+    def test_invalid_input_means_nothing_saved_to_db(self):
+        self.post_invalid_input()
+        self.assertEqual(Item.objects.all().count(), 0)
+
+    def test_invalid_input_renders_list_template(self):
+        response = self.post_invalid_input()
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_invalid_input_renders_form_with_errors(self):
+        response = self.post_invalid_input()
+        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertContains(response, escape(EMPTY_LIST_ERROR))
 
     def test_saving_a_POST_request(self):
         self.client.post(
